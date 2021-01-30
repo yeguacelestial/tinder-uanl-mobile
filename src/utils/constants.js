b87b65a50ab3c9bfd71c1833ee3a5d2e1a60f6a5
Yegua Celestial <yeguacelestial@gmail.com>
2021-01-30 00:18:32 -0600

    Change Card size

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@ const { width, height } = Dimensions.get('screen');
 
 export const CARD = {
   WIDTH: width * 0.9,
-  HEIGHT: height * 0.75,
+  HEIGHT: height * 0.65,
   BORDER_RADIUS: 20,
   OUT_OF_SCREEN: width * 0.5 * width,
 };
